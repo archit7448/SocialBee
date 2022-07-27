8c45893103fe673923c02ec66989432e16d7bb93
archit singh <87135461+archit7448@users.noreply.github.com>
2022-07-27 10:52:46 +0530

    delete css

--- src/utility/CSS/ReverseArray/reverseArray.js
+++
@@ -1,7 +0,0 @@
-export const reverseArrayFunc = (array) => {
-  let reverseArray = [];
-  for (let i = array.length - 1; i >= 0; i--) {
-    reverseArray.push(array[i]);
-  }
-  return reverseArray;
-};
