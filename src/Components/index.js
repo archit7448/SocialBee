a764b16f77368fc170c448fc5a36050ec7741cbe
archit singh <87135461+archit7448@users.noreply.github.com>
2022-07-27 10:47:29 +0530

    fix naming

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { Sidebar } from "./Sidebar/sidebar";
-export { Post } from "./Post/post";
-export { FollowSidebar } from "./FollowSidebar/FollowSidebar";
-export { Modal } from "./Modal/modal";
-export { Profile } from "./Profile/Profile";
